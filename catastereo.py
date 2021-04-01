513d3d4394485a785a37d5638a5c9cbaa3d4bc79
bornandd <d.bornand@gmx.ch>
2021-04-01 14:24:25 +0200

    Essential Matrix computation

--- catastereo.py
+++ catastereo.py
@@ -54,8 +54,8 @@ new_width = x # min(x, width-x)
 img[:,x+1+new_width, :] = 0
 
 # crop images and make grayscale
-imgL = cv2.cvtColor(img[:,:x,:], cv2.COLOR_BGR2GRAY)
-imgR = cv2.cvtColor(img[:,x+1:x+1+new_width,:], cv2.COLOR_BGR2GRAY)
+imgR = cv2.cvtColor(img[:,:x,:], cv2.COLOR_BGR2GRAY)
+imgL = cv2.cvtColor(img[:,x+1:x+1+new_width,:], cv2.COLOR_BGR2GRAY)
 
 cv2.imshow('select border', img)
 #cv2.imshow('imgL', imgL)
@@ -86,33 +86,10 @@ bf = cv2.BFMatcher()
 matches = bf.knnMatch(des1, des2, k=2)
 
 ########### (https://docs.opencv.org/master/da/de9/tutorial_py_epipolar_geometry.html)
-FLANN_INDEX_KDTREE = 1
-index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-search_params = dict(checks=50)
-flann = cv2.FlannBasedMatcher(index_params, search_params)
-matches = flann.knnMatch(des1, des2, k=2)
-pts1 = []
-pts2 = []
-# ratio test as per Lowe's paper
-for i, (m, n) in enumerate(matches):
-    if m.distance < 0.8*n.distance:
-        pts2.append(kp2[m.trainIdx].pt)
-        pts1.append(kp1[m.queryIdx].pt)
-
-pts1 = np.int32(pts1)
-pts2 = np.int32(pts2)
-F, mask = cv2.findFundamentalMat(pts1, pts2, cv2.FM_LMEDS)
-
-# We select only inlier points
-pts1 = pts1[mask.ravel()==1]
-pts2 = pts2[mask.ravel()==1]
-
-print('Im on line 109')
-
-def drawlines(img1,img2,lines,pts1,pts2):
+def drawlines(img1, img2, lines, pts1, pts2):
     ''' img1 - image on which we draw the epilines for the points in img2
         lines - corresponding epilines '''
-    r,c = img1.shape
+    r, c = img1.shape
     img1 = cv2.cvtColor(img1, cv2.COLOR_GRAY2BGR)
     img2 = cv2.cvtColor(img2, cv2.COLOR_GRAY2BGR)
     for r, pt1, pt2 in zip(lines,pts1,pts2):
@@ -124,17 +101,47 @@ def drawlines(img1,img2,lines,pts1,pts2):
         img2 = cv2.circle(img2, tuple(pt2), 5, color, -1)
     return img1, img2
 
+FLANN_INDEX_KDTREE = 1
+index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+search_params = dict(checks=50)
+flann = cv2.FlannBasedMatcher(index_params, search_params)
+matches = flann.knnMatch(des1, des2, k=2)
+pts1 = []
+pts2 = []
+
+# ratio test as per Lowe's paper
+for i, (m, n) in enumerate(matches):
+    if m.distance < 0.8*n.distance:
+        pts2.append(kp2[m.trainIdx].pt)
+        pts1.append(kp1[m.queryIdx].pt)
+
+pts1 = np.int32(pts1)
+pts2 = np.int32(pts2)
+F, mask = cv2.findFundamentalMat(pts1, pts2, cv2.FM_LMEDS)
+
+# (https://stackoverflow.com/questions/59014376/what-do-i-do-with-the-fundamental-matrix)
+
+# We select only inlier points
+pts1 = pts1[mask.ravel() == 1]
+pts2 = pts2[mask.ravel() == 1]
+
+# Essential from Fundamental
+# (https://docs.opencv.org/master/d9/d0c/group__calib3d.html#ga0c86f6478f36d5be6e450751bbf4fec0)
+E, mask2 = cv2.findEssentialMat(pts1, pts2, cameraMatrix=K)
+print(E)
+print(mask2)
+
 # Find epilines corresponding to points in right image (second image) and
 # drawing its lines on left image
-lines1 = cv2.computeCorrespondEpilines(pts2.reshape(-1,1,2), 2,F)
+lines1 = cv2.computeCorrespondEpilines(pts2.reshape(-1, 1, 2), 2, F)
 lines1 = lines1.reshape(-1, 3)
-img5, img6 = drawlines(imgR, imgL, lines1, pts1, pts2)
+img5, img6 = drawlines(imgL, imgR, lines1, pts1, pts2)
 
 # Find epilines corresponding to points in left image (first image) and
 # drawing its lines on right image
 lines2 = cv2.computeCorrespondEpilines(pts1.reshape(-1, 1, 2), 1, F)
 lines2 = lines2.reshape(-1, 3)
-img3, img4 = drawlines(imgL, imgR, lines2, pts2, pts1)
+img3, img4 = drawlines(imgR, imgL, lines2, pts2, pts1)
 plt.subplot(121), plt.imshow(img5)
 plt.subplot(122), plt.imshow(img3)
 plt.show()
@@ -148,7 +155,7 @@ for m, n in matches:
         good.append([m])
 
 # cv2.drawMatchesKnn expects list of lists as matches.
-img3 = cv2.drawMatchesKnn(imgL, kp1, imgR, kp2, good, flags=2, outImg=None)
+img3 = cv2.drawMatchesKnn(imgR, kp1, imgR, kp2, good, flags=2, outImg=None)
 
 point = kp1[1].pt
 for x in kp1:
@@ -178,7 +185,7 @@ print('SIFT_detector has ended: ')
 # Disparity Computation 
 ###############################################################################
 stereo = cv2.StereoSGBM_create(numDisparities=50, blockSize=8, speckleRange=50, speckleWindowSize=30, uniquenessRatio=9)
-disparity = stereo.compute(imgR,imgL)
+disparity = stereo.compute(imgL,imgR)
 print('disparity.shape = ' + str(disparity.shape))
 
 #cv2.namedWindow('disparity map', cv2.WINDOW_NORMAL)
